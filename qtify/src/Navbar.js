9c22f1171ddf5fd269820ada971bb32417ec9fc4
Deena-toobler <deena.poulose@zuseapps.co.uk>
2025-03-23 15:39:04 +0530

    test qtify

--- qtify/src/Navbar.js
+++ qtify/src/Navbar.js
@@ -1,14 +1,31 @@
+// import styles from "./HomePage.module.css";
+
+// const Navbar = () => (
+//   <nav className={styles.navbar}>
+//     <div className={styles.logo}>QTify</div>
+//     <input
+//       type="text"
+//       placeholder="search" /* Ensure lowercase 'search' */
+//       className={styles.searchBar}
+//     />
+//     <button className={styles.feedbackButton}>Give Feedback</button>
+//   </nav>
+// );
+// export default Navbar;
 import styles from "./HomePage.module.css";
 
 const Navbar = () => (
   <nav className={styles.navbar}>
-    <div className={styles.logo}>QTify</div>
+    <div className={styles.logoWrapper}>
+      <img src="/logo.png" alt="QTify Logo" className={styles.logo} />
+    </div>
     <input
       type="text"
-      placeholder="search" /* Ensure lowercase 'search' */
+      placeholder="Search a song | Search an album" /* Ensure correct case */
       className={styles.searchBar}
     />
     <button className={styles.feedbackButton}>Give Feedback</button>
   </nav>
 );
+
 export default Navbar;
