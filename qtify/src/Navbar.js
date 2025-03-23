730749ba687fe73ed62a8c96dd5c8b595977f2b7
Deena-toobler <deena.poulose@zuseapps.co.uk>
2025-03-23 15:28:57 +0530

    test qtify

--- qtify/src/Navbar.js
+++ qtify/src/Navbar.js
@@ -1,11 +1,14 @@
-
 import styles from "./HomePage.module.css";
 
 const Navbar = () => (
-    <nav className={styles.navbar}>
-      <div className={styles.logo}>QTify</div>
-      <input type="text" placeholder="Search for albums or songs" className={styles.searchBar} />
-      <button className={styles.feedbackButton}>Give Feedback</button>
-    </nav>
-  );
-  export default Navbar;
+  <nav className={styles.navbar}>
+    <div className={styles.logo}>QTify</div>
+    <input
+      type="text"
+      placeholder="search" /* Ensure lowercase 'search' */
+      className={styles.searchBar}
+    />
+    <button className={styles.feedbackButton}>Give Feedback</button>
+  </nav>
+);
+export default Navbar;
